a258db67f888b295e29d7fdc02f2accc1fc00f9f
Rajeeb Kumar Malik <rkm2541997@gmail.com>
2022-03-14 00:44:49 +0530

    Duplicate files are removed

--- migrations/1_initial_mintNFT.js
+++
@@ -1,8 +0,0 @@
-const mintContractNFT721 = artifacts.require("mintContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(mintContractNFT721);
-};
-//module.exports is basically a instructions that tell Node.js, which bit of code to export so that other files in the
-//project folder can access that code
-//Here, deployed smart contract will be accessed by other files as well
